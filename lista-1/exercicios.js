ce246ec4ffb99ee51a83533afa533644b6546ad4
Carlos Eduardo Bueno de Souza <carlosbueno_53@hotmail.com>
2025-08-14 04:20:09 +0000

    teste

---
+++ lista-1/exercicios.js
@@ -0,0 +1,18 @@
+let input = require('readline-sync');
+// 
+let n1 = parseInt( input.question("Digite um numero: "))
+
+
+//exibir - saida
+console.log("O numero digitado é: ", n1);
+//retangulo processamento 
+n1 = n1 + 2
+console.log("O numero digitado é: " , n1);
+//retangulo processamento
+n1 = n1 - 1
+console.log("O numero digitado é: " , n1);
+//retangulo processamento
+n1 = n1 * 5 
+console.log("O numero digitado é: " . n1);
+n1 = n1 / 2
+
